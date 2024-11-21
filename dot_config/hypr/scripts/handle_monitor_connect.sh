230e353931361bef4ce8dd0d65ba5f842a3a3ded
TosakRin@HPArch <TosakRin@outlook.com>
2024-11-21 16:22:04 +0800

    feat: config init

---
+++ dot_config/hypr/scripts/handle_monitor_connect.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# if you want workspaces to automatically go to a monitor upon connection, use the following:
+# Example: This makes workspaces 1, 2, 4, and 5 go to monitor 1 when connecting it.
+
+handle() {
+  case $1 in monitoradded*)
+    hyprctl dispatch moveworkspacetomonitor "1 1"
+    hyprctl dispatch moveworkspacetomonitor "2 1"
+    hyprctl dispatch moveworkspacetomonitor "4 1"
+    hyprctl dispatch moveworkspacetomonitor "5 1"
+  esac
+}
+
+socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" | while read -r line; do handle "$line"; done
---
+++ dot_config/zsh/modern_unix.sh
@@ -0,0 +1,19 @@
+# see in repo: https://github.com/ibraheemdev/modern-unix
+# https://github.com/johnalanwoods/maintained-modern-unix
+
+# Replace ls with exa
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l='eza -lah --color=always --group-directories-first --icons' # tree listing
+
+alias df=duf
+alias cat=bat
+alias du=dust
+alias tree=broot
+alias grep=rg
+alias ps=procs
+alias top=btop
+alias find=fd
+alias ping=gping
